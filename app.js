d43852329a2c683fd0bf9aeb13ba91ba77e54906
Qamar Abbas <reply.qamar@gmail.com>
2023-05-10 19:56:29 +0500

    initialized empty project

---
+++ app.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+require('dotenv').config();
+var mongoose = require('mongoose');
+var http = require('http').Server(app);
+var path = require('path');
+var io = require('socket.io')(http);
+
+// connecting to MongoDB
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`Connected to MongoDB at ${process.env.MONGO_URL}`);
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
+
+// starting the server
+const port = 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
